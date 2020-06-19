1b9f5d6f227121649a86265844a79903c488ea25
WillDener <42719251+WillDener@users.noreply.github.com>
2020-06-19 20:10:57 -0300

    tarefa fabricio

---
+++ aluguel.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+
+def aluguel(valor, dia):
+    r = requests.get(
+        'https://aluguebug.herokuapp.com/calc?dados={"valor_nominal":'+str(valor)+',"dia":'+str(dia)+'}')
+    return json.loads(r.json())
+
+
+print('Willian Dener da Silva Pinto \nAplicacao em esta consumindo a API Web \nhttps://aluguebug.herokuapp.com/calc?dados')
---
+++ testes.py
@@ -0,0 +1,60 @@
+import pytest
+import aluguel as z
+
+
+def teste_menor_1_WDSP():
+    valor = z.aluguel(350.0, -1)
+    esperado = -1
+    assert valor['valor_calculado'] == esperado
+
+
+def teste_dia_1_WDSP():
+    valor = z.aluguel(350.0, 1)
+    valor_descontado = 350.0-(350.0*0.10)
+    assert valor['valor_calculado'] == valor_descontado
+
+
+def teste_dia_0_WDSP():
+    valor = z.aluguel(350.0, 0)
+    esperado = -1
+    assert valor['valor_calculado'] == esperado
+
+
+def teste_dia_6_WDSP():
+    valor = z.aluguel(350.0, 6)
+    valor_descontado = 350.0-(350.0*0.05)
+    assert valor['valor_calculado'] == valor_descontado
+
+
+def teste_dia_11_WDSP():
+    valor = z.aluguel(350.0, 11)
+    valor_descontado = 350.0-(350.0*0.0)
+    assert valor['valor_calculado'] == valor_descontado
+
+
+def teste_dia_16_WDSP():
+    valor_nominal = 350.0
+    valor = z.aluguel(valor_nominal, 16)
+    dia_juro = 16 - 15
+    valor_descontado = 350.0+(350.0*0.02)+(350.0*(dia_juro/1000))
+    assert valor['valor_calculado'] == valor_descontado
+
+
+def teste_dia_30_WDSP():
+    valor_nominal = 350.0
+    valor = z.aluguel(valor_nominal, 30)
+    dia_juro = 30 - 15
+    valor_descontado = 350.0+(350.0*0.02)+(350.0*(dia_juro/1000))
+    assert valor['valor_calculado'] == valor_descontado
+
+
+def teste_maior_30_WDSP():
+    valor_nominal = 350.0
+    valor = z.aluguel(valor_nominal, 35)
+    esperado = -1
+    assert valor['valor_calculado'] == esperado
+
+# Gerar relatório
+# pytest testes.py --html=report.html
+# Rodar testes
+# pytest testes.py
